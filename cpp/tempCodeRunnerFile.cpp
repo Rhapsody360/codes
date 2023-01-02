85b50fa48564ddd5a7e09e98f42b2c66aac5bb6c
Yash Sonone <yashsonone360@gmail.com>
2023-01-02 14:58:06 +0530

    recursion first try

--- cpp/tempCodeRunnerFile.cpp
+++ cpp/tempCodeRunnerFile.cpp
@@ -1,18 +1,18 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main() {
-    // Write C++ code here
-    string str = "Hello";
-    string asf = "" ;
-    string s1 = "12";
-    char ch = '1';
-    // std::cout << asf + (char)(stoi(s1) + 'a' - 1);
-    std :: cout << s1 + ch << endl;
-    std :: cout << ch + s1 << endl;
-    std :: cout << ch + str << endl;
-    std :: cout << (str + ch) << endl;
-
-    std :: cout << ("" + '1') << endl;;
-    
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int b;
+        cin >> b;
+        if (b <=3)
+            cout << 0 << endl;
+        else
+            cout << (b/2)*(b/2)-(b/2)-((b/2)*(b/2)-(b/2))/2 << endl;
+    }
     return 0;
 }
